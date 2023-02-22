b64bb7ca3453a78e34eaa3eb0a37fabe6184774c
heeyeon <dlgmldus3289@naver.com>
2023-02-22 16:47:55 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,16 +2,17 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FixDiscountPolicy implements DiscountPolicy {
-    private int discountFixAmout = 1000;    //1000원 할인
+    private int discountFixAmount = 1000;    //1000원 할인
 
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {  //enum 타입은 == 사용
-            return discountFixAmout;
+            return discountFixAmount;
         } else {
             return 0;
         }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,13 +8,20 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor //final 붙은 객체를 가지고 생성자를 만들어줌
+//@RequiredArgsConstructor //final 붙은 객체를 가지고 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService{  //인터페이스에만 의존하므로 DIP 지킴
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
